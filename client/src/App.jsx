e5681aae6690e7281a7626f5a6843ddc77732481
Rediat-Andualem <rediatandualeme@gmail.com>
2025-05-09 19:42:27 +0530

    state management

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,11 @@
 import { useState } from 'react'
 import './App.css'
-
+import LogInSignUp from './Page/LogInSignUp/LogInSignUp'
 function App() {
  
   return (
     <>
-     
+     <LogInSignUp/>
     </>
   )
 }
--- client/src/main.jsx
+++ client/src/main.jsx
@@ -2,9 +2,26 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import createStore from 'react-auth-kit/createStore'
+import AuthProvider from 'react-auth-kit';
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+
+const store = createStore({
+	authName:'token',
+	authType:'localStorage',
+	cookieDomain: window.location.hostname,
+	cookieSecure: false
+  });
+
+const root = createRoot(container);
+
+root.render(
+  <AuthProvider
+  store={store}
+ >
+ <BrowserRouter>
+   <App />
+ </BrowserRouter>
+  </AuthProvider>
+);
